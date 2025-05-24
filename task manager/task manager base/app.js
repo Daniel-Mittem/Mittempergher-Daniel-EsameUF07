dfc8a488461b5d419c80b1d27231a001a81fc836
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-05-24 18:34:43 +0200

    feat: implementazione della funzione addTask per creare nuove attività

--- task manager/task manager base/app.js
+++ task manager/task manager base/app.js
@@ -5,4 +5,16 @@ const taskInput = document.getElementById('taskInput');
 const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById('taskList');
        
+function addTask() {
+    const text = taskInput.value.trim();
+        if (text === '') return;
 
+        tasks.push({
+            id: taskId++,
+            text: text,
+            completed: false
+        });
+
+        taskInput.value = '';
+        renderTasks();
+}
