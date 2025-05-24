47ac327b1d8c0cd49f965c011e14f107bacfec4c
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-05-24 18:32:19 +0200

    feat: aggiunta logica di gestione delle attività con app.js e integrazione nell'index.html

---
+++ task manager/task manager base/app.js
@@ -0,0 +1,8 @@
+let tasks = [];
+let taskId = 1;
+
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList = document.getElementById('taskList');
+       
+
