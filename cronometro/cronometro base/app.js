5ff70db006bfd54010ba2016c8f120a4b8801095
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-05-25 21:45:22 +0200

    feat: aggiunta variabili per il controllo del cronometro e collegamento degli elementi DOM

--- cronometro/cronometro base/app.js
+++ cronometro/cronometro base/app.js
@@ -2,3 +2,8 @@ let startTime = 0;
 let elapsedTime = 0;
 let timerInterval = null;
 let isRunning = false;
+
+const timeDisplay = document.getElementById('timeDisplay');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const resetBtn = document.getElementById('resetBtn');
